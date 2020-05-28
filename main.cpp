6be167010e8541cb3d763d337c7899f436639992
drumath <2237k.drumath@gmail.com>
2020-05-28 14:11:23 +0900

    :sparkles: パフォーマンスが上がった気がする

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 #define N 1000
-#define ETA 0.01
+#define ETA 0.05
 
 struct vector2
 {
@@ -9,10 +9,10 @@ struct vector2
 };
 
 // set bezier control points
-vector2 p0 = {0, 0};
-vector2 p1 = {0.5, 0.2};
-vector2 p2 = {0.6, 0.8};
-vector2 p3 = {1, 1};
+vector2 p0 = {-1.0, -1.0};
+vector2 p1 = {0.2, -0.6};
+vector2 p2 = {-0.4, 0.6};
+vector2 p3 = {1.0, 1.0};
 
 vector2 calcBezier(double t)
 {
@@ -21,11 +21,7 @@ vector2 calcBezier(double t)
       p0.y * (1.0 - t) * (1.0 - t) * (1.0 - t) + 3.0 * p1.y * (1.0 - t) * (1.0 - t) * t + 3.0 * p2.y * (1.0 - t) * t * t + p3.y * t * t * t};
 }
 
-double calcYChilda(double a0, double a1, double a2, double a3, double x)
-{
-  return a0 * x * x * x + a1 * x * x + a2 * x + a3;
-}
-double calcYChilda4d(double a0, double a1, double a2, double a3, double a4, double x)
+inline double calcYChilda4d(double a0, double a1, double a2, double a3, double a4, double x)
 {
   return a0 * x * x * x * x + a1 * x * x * x + a2 * x * x + a3 * x + a4;
 }
@@ -39,7 +35,7 @@ int main()
   double a3 = 5.0;
   double a4 = 5.0;
 
-  for (int k = 0; k < 1000000; k++)
+  for (int k = 0; k < 100000; k++)
   {
 #pragma region a0
     double a0_new;
@@ -47,7 +43,8 @@ int main()
     for (int i = 0; i < N; i++)
     {
       double t_i = (double)i / (double)N;
-      dE_da0 += (calcBezier(t_i).y - calcYChilda4d(a0, a1, a2, a3, a4, calcBezier(t_i).x)) * calcBezier(t_i).x * calcBezier(t_i).x * calcBezier(t_i).x* calcBezier(t_i).x;
+      vector2 b = calcBezier(t_i);
+      dE_da0 += (b.y - calcYChilda4d(a0, a1, a2, a3, a4, b.x)) * b.x * b.x * b.x* b.x;
     }
     dE_da0 *= -2.0 / (double)N;
     a0_new = a0 - ETA * dE_da0;
@@ -60,7 +57,8 @@ int main()
     for (int i = 0; i < N; i++)
     {
       double t_i = (double)i / (double)N;
-      dE_da1 += (calcBezier(t_i).y - calcYChilda4d(a0, a1, a2, a3, a4, calcBezier(t_i).x)) * calcBezier(t_i).x * calcBezier(t_i).x* calcBezier(t_i).x;
+      vector2 b = calcBezier(t_i);
+      dE_da1 += (b.y - calcYChilda4d(a0, a1, a2, a3, a4, b.x)) * b.x * b.x* b.x;
     }
     dE_da1 *= -2.0 / (double)N;
     a1_new = a1 - ETA * dE_da1;
@@ -73,7 +71,8 @@ int main()
     for (int i = 0; i < N; i++)
     {
       double t_i = (double)i / (double)N;
-      dE_da2 += (calcBezier(t_i).y - calcYChilda4d(a0, a1, a2, a3, a4, calcBezier(t_i).x)) * calcBezier(t_i).x * calcBezier(t_i).x;
+      vector2 b = calcBezier(t_i);
+      dE_da2 += (b.y - calcYChilda4d(a0, a1, a2, a3, a4, b.x)) * b.x * b.x;
     }
     dE_da2 *= -2.0 / (double)N;
     a2_new = a2 - ETA * dE_da2;
@@ -86,7 +85,8 @@ int main()
     for (int i = 0; i < N; i++)
     {
       double t_i = (double)i / (double)N;
-      dE_da3 += (calcBezier(t_i).y - calcYChilda4d(a0, a1, a2, a3, a4, calcBezier(t_i).x))* calcBezier(t_i).x;
+      vector2 b = calcBezier(t_i);
+      dE_da3 += (b.y - calcYChilda4d(a0, a1, a2, a3, a4, b.x))* b.x;
     }
     dE_da3 *= -2.0 / (double)N;
     a3_new = a3 - ETA * dE_da3;
@@ -99,15 +99,14 @@ int main()
     for (int i = 0; i < N; i++)
     {
       double t_i = (double)i / (double)N;
-      dE_da4 += (calcBezier(t_i).y - calcYChilda4d(a0, a1, a2, a3, a4, calcBezier(t_i).x));
+      vector2 b = calcBezier(t_i);
+      dE_da4 += (b.y - calcYChilda4d(a0, a1, a2, a3, a4, b.x));
     }
     dE_da4 *= -2.0 / (double)N;
     a4_new = a4 - ETA * dE_da4;
     a4 = a4_new;
 #pragma endregion a4
 
-
-    // std::cout << a0 << " " << a1 << " " << a2 << " " << a3 << std::endl;
   }
 
   std::cout << a0 << " " << a1 << " " << a2 << " " << a3 << " " << a4 << std::endl;
